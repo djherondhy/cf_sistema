caabc0e62ecb97cb4a78347944bf5e313e35b39f
jhorlen <jhorlenpitch@gmail.com>
2023-05-16 18:58:34 -0400

    refactor(#11) - adição imagem icone usuário e ajustes nas mesmas

--- src/views/login-user.php
+++ src/views/login-user.php
@@ -11,20 +11,22 @@
     <div class="container">
         <div class="login-content">
             <div class="login-left">
+                
                 <div class="text-agroup">
-                <h2>Bem Vindo ao ConnectFarma</h2>
-                </div>
+                    <h2>Olá Cliente! Bem Vindo ao ConnectFarma!</h2>
+                </div>    
 
                 <div class="img_login">
-                    <img src="pictures/entregador.png">
+                    <img src="pictures/user.png">
                 </div>
+
                 <div class="text-agroup">
                 <p>Os melhores produtos farmacêuticos ao seu alcance, faça seu login escolha seu produto e receba onde quiser.</p>
                 </div>
             </div>
             <div class="login-right">
                 <form action="">
-                <img src="pictures/entregador.png">
+                <img src="pictures/user.png">
                     <p class="title">User Login</p>
                     <div class="input-agroupt">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path></svg>
