946e6e027df8ae0d1e37cc96ad1e507b6e716c62
Djherondhy <djherondhyoficial@gmail.com>
2023-05-16 15:09:02 -0400

    feat(#12): :lipstick: Adição de responsividade ao tela de login entregador

--- src/views/login-entregador.php
+++ src/views/login-entregador.php
@@ -16,7 +16,7 @@
                     <h2>Olá entregador! Bem Vindo!</h2>
                 </div>    
 
-                <div class="img_entregador">
+                <div class="img_login">
                     <img src="pictures/entregador.png">
                 </div>
 
@@ -27,6 +27,7 @@
             </div>
             <div class="login-right">
                 <form action="">
+                    <img src="pictures/entregador.png">
                     <p class="title">Entregador Login</p>
                     <div class="input-agroupt">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path></svg>
