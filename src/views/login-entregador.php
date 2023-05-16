bd6ea332b03923d85d91d16e5c36d43a7840a587
jhorlen <jhorlenpitch@gmail.com>
2023-05-16 15:11:25 -0400

    frontend(#13) - Criação de tela login e arquivo css baseado no do cliente, e adição de imagems na tela de login entregador

--- src/views/login-entregador.php
+++ src/views/login-entregador.php
@@ -4,9 +4,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Entregador</title>
+    <title>Vamos Lá, Login</title>
+    <link rel="stylesheet" href="login-entregador.css">
 </head>
 <body>
-    #login entregador 2
+    <div class="container">
+        <div class="login-content">
+            <div class="login-left">
+
+                <div class="text-agroup">
+                    <h2>Olá entregador! Bem Vindo!</h2>
+                </div>    
+
+                <div class="img_entregador">
+                    <img src="pictures/entregador.png">
+                </div>
+
+                <div class="text-agroup">
+                <p>Somos a melhor parceria da sua vida! Não somos uma empresa, somos uma familia!</p>
+                </div>
+              
+            </div>
+            <div class="login-right">
+                <form action="">
+                    <p class="title">Entregador Login</p>
+                    <div class="input-agroupt">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="transform: ;msFilter:;"><path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path></svg>
+                        <input type="text" placeholder="Digite seu e-mail">
+                    </div>
+                    <div class="input-agroupt">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="transform: ;msFilter:;"><path d="M17 8V7c0-2.757-2.243-5-5-5S7 4.243 7 7v3H6c-1.103 0-2 .897-2 2v8c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-8c0-1.103-.897-2-2-2H9V7c0-1.654 1.346-3 3-3s3 1.346 3 3v1h2zm1 4 .002 8H6v-8h12z"></path></svg>
+
+                    <input type="text" placeholder="Digite sua senha">
+
+                    </div>
+                    <div class="forget">
+                        <a href="#">Esqueceu sua Senha?</a>
+                    </div>
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
