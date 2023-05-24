555ccaf5502c785abd63eda3f88ffc9f4cb5f766
jhorlen <jhorlenpitch@gmail.com>
2023-05-24 08:28:29 -0400

    refactor(#11) - atualização gif icone usuário

--- src/views/login-user.php
+++ src/views/login-user.php
@@ -17,7 +17,7 @@
                 </div>    
 
                 <div class="img_login">
-                    <img src="pictures/user.png">
+                    <img src="pictures/clientep.gif">
                 </div>
 
                 <div class="text-agroup">
@@ -26,7 +26,7 @@
             </div>
             <div class="login-right">
                 <form action="">
-                <img src="pictures/user.png">
+                <img src="pictures/clientep.gif">
                     <p class="title">User Login</p>
                     <div class="input-agroupt">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" ><path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path></svg>
